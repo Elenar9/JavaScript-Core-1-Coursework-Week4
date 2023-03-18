644c5ac86ab1de343e156d302a707f8fc64b6a97
Danny Romero <108970600+Elenar9@users.noreply.github.com>
2023-03-18 20:41:53 +0000

    javaScript-Core-1-Coursework-Week4
    update exercise B

--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -20,6 +20,9 @@ let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
 let pairs = pairsByIndex.map(function (indexes) {
+  if(pairsByIndex.some(isNull)){
+    process.exit(1);
+  }
   let student = students[indexes[0]];
   let mentor = mentors[indexes[1]];
   return [student, mentor];
